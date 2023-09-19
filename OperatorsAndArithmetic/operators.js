a64b1f6788780e7677ac6ffb8b4130e9e0d04629
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2023-09-18 19:47:46 -0500

    operators practice learning activities

---
+++ OperatorsAndArithmetic/operators.js
@@ -0,0 +1,58 @@
+// Add Two numbers
+let resultadd
+let num1 = 10;
+let num2 = 2;
+resultadd = num1 + num2;
+console.log(`Sum is ${resultadd}`)
+// Multiply two numbers
+let resultmul
+resultmul = num1 * num2;
+console.log(`Sum is ${resultmul}`)
+//Subtract one from a number
+let resultsubs
+resultsubs = num1 - num2;
+console.log(`Sum is ${resultsubs}`)
+//concatinate two strings together.
+let strresult;
+let str1 = 'Hi';
+let str2 = 'Isaac';
+strresult = str1 + ' ' + str2
+//assign a value to a variable
+let x = 5;
+x += 10;
+//increment the value in a variable by 3
+let y = 2;
+y**=3;
+//compare two values to see if they are the same
+let result;
+if (x == y){
+    result = ("the values are equal")
+}
+else {
+    result = ("The values aren't  equal")
+}
+//check to see if one number is less than another number
+let result2;
+if (x < y){
+    result2 = ("x is less than y")
+}
+else {
+    result2 = ("x is more than y")
+}
+
+//Check to see if two values are NOT equal
+let result3;
+if (x != y){
+    result3 = ('x is not equal to y')
+}
+else{
+    result3 = ('The numbers are equal')
+}
+//check to see if a value is less than 10 and greater than 0
+let result4;
+if (y < 10 &  y >0){
+    result4 = ('True')
+}
+else{
+    result4 = "False"
+}
