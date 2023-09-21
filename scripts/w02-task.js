a21a221ea6b12093b0d29ca4f4ca33bda506dfba
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2023-09-21 09:48:41 -0500

    w02 programming tasks done

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,23 +4,42 @@
 
 /* Step 2 - Variables */
 let fullname = 'Isaac Gutierrez';
-let currentYear = 27;
+let currentYear = 2023;
 let profilePicture = 'images/profilepicture.png';
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.getElementById('image');
+const imageElement = document.querySelector('main#home picture img');
 
 /* Step 4 - Adding Content */
 
-
-
-
-
+nameElement.innerHTML = `<strong>${fullname}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture)
+imageElement.setAttribute = ('alt', `Profile Picture ${fullname}`);
 
 /* Step 5 - Array */
+
+let favFoods = [
+    "Pizza",
+    "Tacos",
+    "Chicken",
+    'Ice Cream',
+]
+foodElement.innerHTML = `${favFoods}`;
+
+let singleFavFood = "Cakes";
+
+favFoods.push(singleFavFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+
+let removeFirtsFood = favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+
+let removeLastFood = favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
 
 
 
