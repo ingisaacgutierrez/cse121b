916aed12807333c15408f9e0db5468ca218878e1
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2023-09-21 08:54:55 -0500

    DOM Basics activity /second time

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,15 +3,15 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+let fullname = 'Isaac Gutierrez';
+let currentYear = 27;
+let profilePicture = 'images/profilepicture.png';
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.getElementById('image');
 
 /* Step 4 - Adding Content */
 
